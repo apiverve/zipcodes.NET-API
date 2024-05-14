9f60661127870c8d586e30b1c49b159aac61fc1d
APIVerve Team <hello@apiverve.com>
2024-05-14 15:47:06 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,51 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class foundCities
+{
+    [JsonProperty("zipcode")]
+    public string zipcode { get; set; }
+
+    [JsonProperty("state_abbr")]
+    public string stateabbr { get; set; }
+
+    [JsonProperty("latitude")]
+    public string latitude { get; set; }
+
+    [JsonProperty("longitude")]
+    public string longitude { get; set; }
+
+    [JsonProperty("city")]
+    public string city { get; set; }
+
+    [JsonProperty("state")]
+    public string state { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("search")]
+    public string search { get; set; }
+
+    [JsonProperty("foundCities")]
+    public foundCities[] foundCities { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
