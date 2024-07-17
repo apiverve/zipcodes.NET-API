e8fced12eae94a6911c1600bb7dfefb14589b6ac
APIVerve Team <hello@apiverve.com>
2024-07-17 12:36:59 -0500

    Bug Fixes

--- zipcodesQueryOptions.cs
+++ zipcodesQueryOptions.cs
@@ -5,8 +5,7 @@ using System.Text;
 namespace APIVerve
 {
     public class zipcodesQueryOptions {
-public string city { get; set; }
-public string state { get; set; }
+public string zip { get; set; }
 }
 
 }
