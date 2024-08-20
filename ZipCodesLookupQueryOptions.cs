25a640e5aa9c60696837ac720c9589eca92d4173
APIVerve Team <hello@apiverve.com>
2024-08-20 13:37:38 -0500

    Bug Fixes

---
+++ ZipCodesLookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ZipCodesLookupQueryOptions {
+public string zip { get; set; }
+}
+
+}
